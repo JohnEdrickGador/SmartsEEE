c91f987e5834e71fb77175a80377fbe473f05273
JohnEdrickGador <jmgador@up.edu.ph>
2023-06-07 15:36:53 +0800

    initialized fields in main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+const gyro_alert_field = document.getElementById('gyro-alert');
+const temp_data_field = document.getElementById('temp-data');
+const humidity_field = document.getElementById('humidity-data');
+const light_field = document.getElementById('light-data');
