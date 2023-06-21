fa2e416190ef8606341ed806e0d0d6556cd7f3f7
JohnEdrickGador <jmgador@up.edu.ph>
2023-06-22 01:09:26 +0800

    Enclosed fields inside a form element

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const emailHandler = (e) => {
+        e.preventDefault();
+        setEmail(e.target.value);
+    }
+
+    const passwordHandler = (e) => {
+        e.preventDefault();
+        setPassword(e.target.value);
+    }
+
+    const signIn = () => {
+
+    }
     return (
         <div className="login-page">
             <div className="login-container">
             <h1 className="login-title">Log in with an admin account</h1>
-                <input type="text" name="email" id="email" placeholder="Email Address"></input>
-                <input type = "password" name="password" id="password" placeholder="Password"></input>
-                <button className="login-btn">Login</button>
-                <a href="#" className="account-apply">No account yet? Apply here</a>
+                <form className="login-form">
+                    <input type = "text" name = "email" id = "email" placeholder = "Email Address" value = {email} onChange={emailHandler}></input>
+                    <input type = "password" name = "password" id = "password" placeholder = "Password" value = {password} onChange={passwordHandler}></input>
+                    <button type = "submit" className = "login-btn">Login</button>
+                    <a href = "#" className = "account-apply">No account yet? Apply here</a>
+                </form>
             </div>
         </div>
     )
