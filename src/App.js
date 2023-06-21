106a7265cb14a41894275f03e73f48a483b4f0aa
JohnEdrickGador <jmgador@up.edu.ph>
2023-06-21 23:53:23 +0800

    started creating login screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-
+      <h1>Sanity Check to see if the app is working</h1>
     </div>
   );
 }
---
+++ src/pages/Login.js
@@ -0,0 +1,10 @@
+function Login() {
+    return (
+        <div className="login-container">
+            <h1>Log in with an admin account</h1>
+            <form action>
+
+            </form>
+        </div>
+    )
+}
