5f5cd4ca394059cd2bfa800349002c1c088e7d9c
JohnEdrickGador <jmgador@up.edu.ph>
2023-06-22 02:06:56 +0800

    Finished writing private route for authentication

---
+++ src/privateroute.js
@@ -0,0 +1,10 @@
+import React, { useContext } from 'react';
+import { Route, Redirect } from "react-router-dom";
+import { AuthContext } from "./auth";
+
+export default function PrivateRoute({component: RouteComponent, ...rest}) {
+    const {currentUser} = useContext(AuthContext);
+    return (
+        <Route {...rest} render = {routeProps => !!currentUser ? (<RouteComponent {...routeProps}/>) : (<Redirect to={"/login"}/>)}/>
+    )
+}
