62e436d591440028008d58918f63ad4cedd775fa
JohnEdrickGador <jmgador@up.edu.ph>
2023-06-22 00:50:30 +0800

    finished first pass of frontend code for the login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,17 @@
+import React from "react";
+
 function Login() {
     return (
-        <div className="login-container">
-            <h1>Log in with an admin account</h1>
-            <form action>
-
-            </form>
+        <div className="login-page">
+            <div className="login-container">
+            <h1 className="login-title">Log in with an admin account</h1>
+                <input type="text" name="email" id="email" placeholder="Email Address"></input>
+                <input type = "password" name="password" id="password" placeholder="Password"></input>
+                <button className="login-btn">Login</button>
+                <a href="#" className="account-apply">No account yet? Apply here</a>
+            </div>
         </div>
     )
 }
+
+export default Login
